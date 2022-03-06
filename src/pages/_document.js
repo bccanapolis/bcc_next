a25ced979897a69053863d0cc1ec898647855468
Bruno de Araujo Alves <me@baraus.dev>
2022-03-06 15:19:56 -0300

    adição de opengraph

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,12 +1,34 @@
 import { Head, Html, Main, NextScript } from 'next/document';
+import web from '@/web';
 
 export default function Document() {
-  return (
-    <Html className='scroll-smooth'>
+
+  return (<Html className='scroll-smooth'>
       <Head>
         <link
           href='https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap'
           rel='stylesheet' />
+
+        <meta name='title' content={web.title} />
+        <meta name='description' content={web.description} />
+
+        <meta property='og:type' content='website' />
+        <meta property='og:url' content={web.url} />
+        <meta property='og:title' content={web.title} />
+        <meta property='og:description' content={web.description} />
+        <meta property='og:image' content={`${web.url}img/open_graph_full.png`} />
+        <meta property='og:image:height' content='1200' />
+        <meta property='og:image:width' content='628' />
+
+        <meta property='og:image' content={`${web.url}img/open_graph_squared.png`} />
+        <meta property='og:image:height' content='512' />
+        <meta property='og:image:width' content='512' />
+
+        <meta property='twitter:card' content='summary_large_image' />
+        <meta property='twitter:url' content={web.url} />
+        <meta property='twitter:title' content={web.title} />
+        <meta property='twitter:description' content={web.description} />
+        <meta property='twitter:image' content={`${web.url}img/open_graph_full.png`} />
       </Head>
       <body>
       <Main />
---
+++ src/web.js
@@ -0,0 +1,5 @@
+export default {
+  title: 'Bacharelado em Ciência da Computação - IFG Anápolis',
+  description: 'Informações sobre o curso de bacharelado em Ciência da Computação do IFG Campus Anápolis',
+  url: 'https://bcc.anapolis.ifg.edu.br/'
+};
