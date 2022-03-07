58dafd19d707ea6573e2d367cdd1488f9e2f463a
Bruno de Araujo Alves <me@baraus.dev>
2022-03-06 21:16:34 -0300

    Updates

--- src/pages/pesquisa/gecomp/index.jsx
+++ src/pages/pesquisa/gecomp/index.jsx
@@ -81,7 +81,7 @@ export default function index({ page }) {
       </BannerBreadcrumb>
       <Container>
         <div className='flex flex-wrap md:flex-nowrap gap-4'>
-          <div className='w-full md:w-8/12 prose prose' dangerouslySetInnerHTML={{ __html: page.description }} />
+          <div className='w-full md:w-8/12 prose' dangerouslySetInnerHTML={{ __html: page.description }} />
           <div className='w-full md:w-4/12 flex flex-col gap-4'>
             <div className='bg-gray-50 px-2 py-3 w-full'>
               <p className='text-lg font-semibold mx-4'>Professores Integrantes</p>
