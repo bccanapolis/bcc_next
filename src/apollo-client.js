b3a8343fc812b1ab6d9963a1e266df379e5b13e0
Bruno de Araujo Alves <devbaraus@gmail.com>
2022-03-08 21:43:28 -0300

    Disabled Apollo Client cache

--- src/apollo-client.js
+++ src/apollo-client.js
@@ -5,11 +5,9 @@ import { ApolloClient, InMemoryCache } from '@apollo/client';
 const defaultOptions = {
   watchQuery: {
     fetchPolicy: 'no-cache',
-    errorPolicy: 'ignore'
   },
   query: {
     fetchPolicy: 'no-cache',
-    errorPolicy: 'all'
   }
 };
 
