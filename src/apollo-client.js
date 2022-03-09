1e94d14544255a52b72e72f3b08c2b335953a2f9
Bruno de Araujo Alves <devbaraus@gmail.com>
2022-03-08 21:38:55 -0300

    Disabled Apollo Client cache

--- src/apollo-client.js
+++ src/apollo-client.js
@@ -2,9 +2,21 @@
 
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
+const defaultOptions = {
+  watchQuery: {
+    fetchPolicy: 'no-cache',
+    errorPolicy: 'ignore'
+  },
+  query: {
+    fetchPolicy: 'no-cache',
+    errorPolicy: 'all'
+  }
+};
+
 const client = new ApolloClient({
   uri: 'https://api.bcc.anapolis.ifg.edu.br/graphql',
-  cache: new InMemoryCache()
-})
+  cache: new InMemoryCache(),
+  defaultOptions
+});
 
-export default client
+export default client;
