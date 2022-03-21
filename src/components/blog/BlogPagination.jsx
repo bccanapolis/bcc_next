2bd3139c4422c4fe145b4a25942932933cf40c4d
Bruno de Araujo Alves <me@baraus.dev>
2022-03-20 23:04:04 -0300

    Melhoria filtros blog

--- src/components/blog/BlogPagination.jsx
+++ src/components/blog/BlogPagination.jsx
@@ -6,7 +6,7 @@ export default function BlogPagination({ searchPosts, page }) {
     <nav aria-label='Navegação da Página' className='flex items-center justify-center w-full'>
       <ul className='inline-flex items-center -space-x-px'>
         <li className={classNames(page.page <= 1 && 'invisible')}>
-          <button onClick={() => searchPosts({ nextPage: page.currentPage + 1 })}
+          <button onClick={() => searchPosts({ nextPage: page.page - 1 })}
                   className={'block py-2.5 px-2 ml-0 leading-tight text-neutral-500 hover:text-white hover:bg-primary'}>
             <span className='sr-only'>Previous</span>
             <ChevronLeftIcon className='w-5 h-5' />
@@ -17,7 +17,7 @@ export default function BlogPagination({ searchPosts, page }) {
                   className={classNames('py-2 px-3 leading-tight hover:text-white hover:bg-primary ', index + 1 == page.currentPage ? 'text-white bg-primary' : 'text-neutral-500')}>{index + 1}</button>
         </li>))}
         <li className={classNames(page.page == page.maxPages && 'invisible')}>
-          <button onClick={() => searchPosts({ nextPage: page.currentPage - 1 })}
+          <button onClick={() => searchPosts({ nextPage: page.page + 1 })}
                   className='block py-2.5 px-2 leading-tight text-neutral-500 hover:text-white hover:bg-primary'>
             <span className='sr-only'>Next</span>
             <ChevronRightIcon className='w-5 h-5' />
