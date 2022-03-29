0314639e64446f22d829de3fdd9cd88f646b5c8a
Bruno de Araujo Alves <me@baraus.dev>
2022-03-29 18:07:17 -0300

    Alterado o nome da variavel de ambiente

--- src/lib/lattes.js
+++ src/lib/lattes.js
@@ -1,3 +1,3 @@
 export async function fetchProfessor(lattes) {
-  return (await fetch(`${process.env.API_IFGPRODUZ}/api/informacoes_docentes?infor_docentes=informacoes_docentesProducao&lattes_id=http://lattes.cnpq.br/${lattes}`)).json();
+  return (await fetch(`${process.env.NEXT_PUBLIC_API_IFGPRODUZ}/api/informacoes_docentes?infor_docentes=informacoes_docentesProducao&lattes_id=http://lattes.cnpq.br/${lattes}`)).json();
 }
