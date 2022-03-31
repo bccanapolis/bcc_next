a4d3c2673b45a89a0ca1f4ee80b2eb372e8571c9
LuannFelipe <luannf1020@gmail.com>
2022-03-31 14:13:33 -0300

    Ajustes no tailwind e nos console.log

--- src/components/home/GameSection.jsx
+++ src/components/home/GameSection.jsx
@@ -1,5 +1,3 @@
-import Banner from "@/components/layout/Banner";
-import Image from "next/image";
 import Link from "next/link";
 import { Autoplay, Pagination } from "swiper";
 import { Swiper, SwiperSlide } from "swiper/react";
@@ -7,27 +5,24 @@ import { Swiper, SwiperSlide } from "swiper/react";
 export default function GameSection({ section={}, games, className })
 {
   return(
-    <div className="Container">
-      <div className='space-y-2 mb-12'>
+    <div className="container mb-12">
+      <div className="space-y-2 mb-12">
         <h6 className="text-4xl font-bold text-center">{section.title}</h6>
         <p className="font-light text-center">{section.subtitle}</p>
       </div >
-      <div className='space-y-2 mb-12'>
-        <Swiper className='swiper-overflow clear-both'
+      <div>
+        <Swiper className='swiper-overflow'
           spaceBetween={10}
           breakpoints={{
             0: { slidesPerView: 1 },
             640: { slidesPerView: 2 },
             768: { slidesPerView: 3 },
-          
           }}
           autoplay={{
             delay: 5000
           }}
           pagination={{ clickable: true }}
           modules={[Pagination, Autoplay]}
-          onSlideChange={() => console.log('slide change')}
-        onSwiper={(swiper) => console.log(swiper)}
           >
           {
 
