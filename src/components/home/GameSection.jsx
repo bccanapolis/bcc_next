6f3166a185f31ea102d29ab2ce47dc6599efb6a8
[Code Tower]Luann Felipe <luannf1020@gmail.com>
2022-03-30 21:36:56 -0300

    coloquei o link dos games em um carrossel

--- src/components/home/GameSection.jsx
+++ src/components/home/GameSection.jsx
@@ -1,54 +1,48 @@
-import Banner from '@/components/layout/Banner';
-import { Menu, Transition } from '@headlessui/react';
-import Link from 'next/link';
-import { Fragment } from 'react';
+import Banner from "@/components/layout/Banner";
+import Image from "next/image";
+import Link from "next/link";
+import { Autoplay, Pagination } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
 
-export default function GameSection({ section={}, games, className }) {
-  return (
-    <>
-      <Banner className={className} childPadding={false}
-              images={[{ url: '/img/game_background_3.1.png', alt: 'Paisagem de Jogo' }]}>
-        <div className='container flex flex-col lg:flex-row py-20 gap-y-8'>
-          <div className='w-full lg:w-8/12'>
-            <p className='text-4xl font-bold text-neutral-100'>{section.title || 'Section title.'}</p>
-          </div>
-          <div className='w-full lg:w-4/12 flex items-center lg:justify-end'>
-            <Menu as='div' className='relative w-full lg:w-max'>
-              <Menu.Button
-                className='bg-neutral-100 py-2 px-4 uppercase  font-medium font-sm w-full lg:w-max hover:bg-primary hover:text-neutral-100 transition-colors duration-300'>Acessar</Menu.Button>
-              <Transition
-                as={Fragment}
-                enter='transition ease-out duration-100'
-                enterFrom='transform opacity-0 scale-95'
-                enterTo='transform opacity-100 scale-100'
-                leave='transition ease-in duration-75'
-                leaveFrom='transform opacity-100 scale-100'
-                leaveTo='transform opacity-0 scale-95'
-              >
-                <Menu.Items
-                  className='absolute left-0 w-full max-h-64 overflow-hidden overflow-y-auto origin-top-right bg-neutral-100 ring-1 ring-black ring-opacity-5 focus:outline-none'>
-                  <ul>
-                    {
-                      games.map(item =>
-                        <Menu.Item key={`game-section-${item}`}>
-                          <li>
-                            <Link href={`/extensao/projetos/games/${item}`}>
-                              <a
-                                className='py-2 px-4 hover:bg-primary hover:text-neutral-100 transition-colors duration-300 inline-flex justify-center w-full'>{item}</a>
-                            </Link>
-                          </li>
-                        </Menu.Item>
-                      )
-                    }
-                  </ul>
-                </Menu.Items>
-              </Transition>
-            </Menu>
+export default function GameSection({ section={}, games, className })
+{
+  return(
+    <div className="Container">
+      <div className='space-y-2 mb-12'>
+        <h6 className="text-4xl font-bold text-center">{section.title}</h6>
+        <p className="font-light text-center">{section.subtitle}</p>
+      </div >
+      <div className='space-y-2 mb-12'>
+        <Swiper className='swiper-overflow clear-both'
+          spaceBetween={10}
+          breakpoints={{
+            0: { slidesPerView: 1 },
+            640: { slidesPerView: 2 },
+            768: { slidesPerView: 3 },
+          
+          }}
+          autoplay={{
+            delay: 5000
+          }}
+          pagination={{ clickable: true }}
+          modules={[Pagination, Autoplay]}
+          onSlideChange={() => console.log('slide change')}
+        onSwiper={(swiper) => console.log(swiper)}
+          >
+          {
 
-          </div>
-        </div>
-      </Banner>
-
-    </>
-  );
+          games.map(game =>(
+            <SwiperSlide key={`game-section-${game}`}  className='flex flex-col items-center group'>{
+              <Link href={`/extensao/projetos/games/${game}`}>
+                <a className="text-3xl font-bold text-slate-600 group-hover:text-primary ">
+                    {game}
+                 </a>
+              </Link>
+            }</SwiperSlide>
+          ))
+          }
+        </Swiper>
+      </div>
+    </div>
+  )
 }
