cdab073a8d5c30d84417062c6b09b64a397550df
LuannFelipe <luannf1020@gmail.com>
2022-03-31 21:48:39 -0300

    Ajustei o container e a cor do ano

--- src/components/home/GameSection.jsx
+++ src/components/home/GameSection.jsx
@@ -5,14 +5,14 @@ import { Swiper, SwiperSlide } from "swiper/react";
 export default function GameSection({ section={}, games, className })
 {
   return(
-    <div className="container mb-12">
+    <div className="container">
       <div className="space-y-2 mb-12">
         <h6 className="text-4xl font-bold text-center">{section.title}</h6>
         <p className="font-light text-center">{section.subtitle}</p>
-      </div >
+      </div>
       <div>
         <Swiper className='swiper-overflow'
-          spaceBetween={10}
+          spaceBetween={20}
           breakpoints={{
             0: { slidesPerView: 1 },
             640: { slidesPerView: 2 },
@@ -29,7 +29,7 @@ export default function GameSection({ section={}, games, className })
           games.map(game =>(
             <SwiperSlide key={`game-section-${game}`}  className='flex flex-col items-center group'>{
               <Link href={`/extensao/projetos/games/${game}`}>
-                <a className="text-3xl font-bold text-slate-600 group-hover:text-primary ">
+                <a className="text-3xl font-bold text-neutral-600 group-hover:text-primary ">
                     {game}
                  </a>
               </Link>
