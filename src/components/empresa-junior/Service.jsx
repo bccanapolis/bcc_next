2681bf0f6bebce63c8529cada8262eab0124fcf4
alnnu <luannf1020@gmail.com>
2022-08-23 14:59:42 -0300

    ajuste nas imagens

--- src/components/empresa-junior/Service.jsx
+++ src/components/empresa-junior/Service.jsx
@@ -1,26 +1,23 @@
-import Link from 'next/link';
-import slugify from 'slugify';
-import { fullName } from '@/utils/user';
-import Image from 'next/image';
-import { apiAsset } from '@/utils';
 import Container from '@/components/layout/Container';
-import { Img } from 'react-image';
+import Image from 'next/image';
 
 export default function Service({ services })
 {
   return(
     <Container >
-      <h2 className="font-bold text-3xl mb-16">Serviços Prestados</h2>
-      <div className='grid grid-cols-1 md:grid-cols-3 gap-x-28'>
+      <h2 className='font-bold text-3xl mb-16'>Serviços Prestados</h2>
+      <div className='grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 gap-x-28'>
         {
           services.map((service, index) => (
             <div key={index}>
-              <Img src={service.photo}/>
+              <div className='relative h-96 w-96'>
+                <Image src={service.photo} layout='fill'/>
+              </div>
               <div className='p-4'>
                 <div>
                   <h5 className='mb-2 text-2xl font-bold tracking-tight text-neutral-900'>
                     {service.title}</h5>
-                  <p className='font-normal text-neutral-700'>{service.description}</p>
+                  <p className='font-normal text-neutral-700 w-96'>{service.description}</p>
                 </div>
               </div>
             </div>
--- src/components/empresa-junior/WhoWeAre.jsx
+++ src/components/empresa-junior/WhoWeAre.jsx
@@ -1,22 +1,20 @@
-import { Img } from 'react-image';
-import { classNames } from '@/utils';
 import Container from '@/components/layout/Container';
-
+import Image from 'next/image';
 export default function WhoWeAre({ className })
 {
   return (
     <>
       <Container>
-        <div className='flex space-x-28 justify-between'>
-          <div className='basis-5/12'>
+        <div className='grid grid-cols-1 lg:grid-cols-2 space-x-28 justify-between'>
+          <div>
             <h2 className='font-bold text-3xl mb-8'>O que é a Code Tower?</h2>
             <div >
-              <p className='font-bold text-xl'> A Code Tower é uma empresa junior especializada no desenvolvimento de sites, sistemas web e de aplicativos, desde o mais simples até um empresarial.</p>
-              <p className='font-bold text-xl'>Nosso maior objetivo é preparar os seus membros para o ambiente do mercado de trabalho, oferecendo treinamento básico necessário para descobrir o seu potêncial como programador, designer e marketing. Se interessou? Venha fazer parte da nossa equipe!</p>
+              <p className='text-xl'> A Code Tower é uma empresa junior especializada no desenvolvimento de sites, sistemas web e de aplicativos, desde o mais simples até um empresarial.</p>
+              <p className='text-xl'>Nosso maior objetivo é preparar os seus membros para o ambiente do mercado de trabalho, oferecendo treinamento básico necessário para descobrir o seu potêncial como programador, designer e marketing. Se interessou? Venha fazer parte da nossa equipe!</p>
             </div>
           </div>
-          <div className='flex'>
-            <Img src={'/img/Code_Tower_logo1.png'} className='self-center'/>
+          <div className='relative'>
+            <Image src={'/img/Code_Tower_logo1.png'} className='self-center' layout='fill'/>
           </div>
         </div>
 
